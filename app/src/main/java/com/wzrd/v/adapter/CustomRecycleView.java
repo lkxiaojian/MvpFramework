60aed8b769ae8958423b6ea173a128a07e4e3c9e
lkxiaojian <18513075404@163.com>
2018-05-09 10:44:50 +0800

    ignore

--- app/src/main/java/com/wzrd/v/adapter/CustomRecycleView.java
+++
@@ -1,8 +0,0 @@
-package com.wzrd.v.adapter;
-
-/**
- * Created by lk on 2017/11/6.
- */
-
-public class CustomRecycleView   {
-}
--- app/src/main/java/com/wzrd/v/view/RecycleResult.java
+++
@@ -1,9 +0,0 @@
-package com.wzrd.v.view;
-
-/**
- * Created by lk on 2017/11/6.
- */
-
-public interface RecycleResult {
-    void last(String message);
-}
